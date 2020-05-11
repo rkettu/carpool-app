0c132c6c4c7d933abbf9f0d8fcdf385faa723896
Roope <roope-kettu@hotmail.com>
2020-05-11 12:09:33 +0300

    Project base added - using API 23 - Android 6.0

---
+++ app/src/main/java/com/example/carpool_app/Ride.java
@@ -0,0 +1,74 @@
+package com.example.carpool_app;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Ride implements Serializable {
+    private String uid;
+    private String duration;
+    private int pickUpDistance;
+    private long leaveTime;
+    private String startAddress;
+    private String endAddress;
+    private int freeSlots;
+    private float price;
+    private List<HashMap<String,String>> points;
+    private List<String> waypointAddresses;
+    private List<String> participants;
+    private double distance;
+    private String startCity;
+    private String endCity;
+
+    // Constructor with all parameters - required for document<->object
+    // Use when creating a ride
+    public Ride(String uid, String duration, long leaveTime,
+                 String startAddress, String endAddress, int freeSlots, float price, double distance,
+                 List<HashMap<String,String>> points, List<String> waypointAddresses,
+                 List<String> participants, int pickUpDistance, String startCity, String endCity)
+    {
+        this.uid = uid;
+        this.duration = duration;
+        this.pickUpDistance = pickUpDistance;
+        this.leaveTime = leaveTime;
+        this.startAddress = startAddress;
+        this.endAddress = endAddress;
+        this.freeSlots = freeSlots;
+        this.price = price;
+        this.points = points;
+        this.waypointAddresses = waypointAddresses;
+        this.participants = participants;
+        this.distance = distance;
+        this.startCity = startCity;
+        this.endCity = endCity;
+    }
+
+    // Getters for all fields - required for document<->object
+    public String getDuration() { return duration; }
+    public long getLeaveTime() { return leaveTime; }
+    public int getPickUpDistance(){ return pickUpDistance; }
+    public String getStartAddress() { return startAddress; }
+    public String getEndAddress() { return endAddress; }
+    public int getFreeSlots() { return freeSlots; }
+    public float getPrice() { return price; }
+    public List<HashMap<String,String>> getPoints() { return points; }
+    public List<String> getWaypointAddresses() { return waypointAddresses; }
+    public List<String> getParticipants() { return participants; }
+    public double getDistance() { return distance; }
+    public String getStartCity() { return startCity; }
+    public String getEndCity() { return endCity; }
+
+    public void removeFreeSlot() {
+        this.freeSlots--;
+    }
+
+    public void addToParticipants(String userId) {
+        participants.add(userId);
+    }
+
+    public void initParticipants()
+    {
+        participants = new ArrayList<>();
+    }
+}
---
+++ app/src/main/java/com/example/carpool_app/User.java
@@ -0,0 +1,78 @@
+package com.example.carpool_app;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class User implements Serializable {
+    private String uid;
+    private String imgUri;
+    private String fname;
+    private String lname;
+    private String email;
+    private String phone;
+    private String bio;
+    private boolean profileCreated;
+    private List<String> bookedRides;
+    private float rating;
+    private int ratingAmount;
+
+    // Constructor with all parameters - required for document<->object
+    // Use when creating a user
+    public User(String fname, String lname, String phone,
+                String email, String bio, String imgUri, String uid,
+                List<String> bookedRides, float rating, int ratingNumber)
+    {
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+        this.phone = phone;
+        this.bio = bio;
+        this.imgUri = imgUri;
+        this.uid = uid;
+        this.bookedRides = bookedRides;
+        this.rating = rating;
+        this.ratingAmount = ratingNumber;
+    }
+    public String getFname() {
+        return fname;
+    }
+    public String getLname() {
+        return lname;
+    }
+    public String getEmail() { return email; }
+    public String getPhone() {
+        return phone;
+    }
+    public String getBio() { return bio; }
+    public String getImgUri() { return imgUri; }
+    public String getUid() { return uid; }
+    public List<String> getBookedRides() { return bookedRides; }
+    public float getRating() { return rating; }
+    public int getRatingAmount() { return ratingAmount; }
+    public boolean getProfileCreated() { return profileCreated; }
+
+    public void setProfCreated(boolean value) {
+        profileCreated = value;
+    }
+
+    public void setUid(String strUid) {
+        uid = strUid;
+    }
+
+    public void setImgUid(String strUri) {
+        imgUri = strUri;
+    }
+
+    public void addToBookedRides(String routeId) {
+        bookedRides.add(routeId);
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public void setRatingAmount(int ratingNumber) {
+        this.ratingAmount = ratingNumber;
+    }
+}
