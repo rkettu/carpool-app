583e0e015333db8ee945eee8203f9e0c576c7a2a
Saku Jamsa <saku.jamsa@gmail.com>
2020-05-11 15:27:11 +0300

    Api key lisätty ( ite key ignoressa ), SetRideActivity xml toimii api29

--- app/src/main/java/com/example/carpool_app/MainActivity.java
+++ app/src/main/java/com/example/carpool_app/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.carpool_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.main_SetRide).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.main_SetRide) {
+            Intent intent = new Intent(this, SetRideActivity.class);
+            startActivity(intent);
+        }
     }
 }
