c0916fae72afe0e847872cc46cfb6fd2b55407f5
Saku Jamsa <saku.jamsa@gmail.com>
2020-05-11 16:38:01 +0300

    Api key lisätty ( ite key ignoressa ), SetRideActivity xml toimii api29, Animaatio toimii,

--- app/src/main/java/com/example/carpool_app/SetRideActivity.java
+++ app/src/main/java/com/example/carpool_app/SetRideActivity.java
@@ -1,17 +1,104 @@
 package com.example.carpool_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 
-public class SetRideActivity extends AppCompatActivity {
+public class SetRideActivity extends AppCompatActivity implements OnMapReadyCallback, View.OnClickListener {
+
+
+    private GoogleMap mMap;
+
+    //Layoutin valikon animaation asetukset
+    Animation ttbAnim, bttAnim;
+    private LinearLayout linearContainer;
+    private ConstraintLayout routeDetails;
+    private Button drawerButton;
+    private ImageButton sijaintiButton;
+    private boolean drawer_expand = true;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_ride);
+
+        //Kartan asetus set_ride_mapViewiin
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.set_ride_mapView);
+        mapFragment.getMapAsync(this);
+
+        //Layoutin valikon animaation asetukset
+        ttbAnim = new AnimationUtils().loadAnimation(this, R.anim.toptobottomanimation);
+        bttAnim = new AnimationUtils().loadAnimation(this, R.anim.bottomtotopanimation);
+        linearContainer = (LinearLayout) findViewById(R.id.set_ride_linearLayout);
+        drawerButton = (Button) findViewById(R.id.set_ride_drawer_bottom);
+        routeDetails = (ConstraintLayout) findViewById(R.id.set_ride_routeDetails);
+        sijaintiButton = findViewById(R.id.set_ride_sijaintiButton);
+        sijaintiButton.setOnClickListener(this);
+        bttAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                linearContainer.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, 0));
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        //Seuraava
+
+    }
+
+    //Layoutin valikon animaation toiminnot
+    public void expandableDrawer(View view) { animationHandler(); }
+    private void animationHandler(){
+        if (!drawer_expand){
+            linearContainer.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            doAnimation(ttbAnim);
+            drawer_expand = true;
+        }else{
+            doAnimation(bttAnim);
+            drawer_expand = false;
+        }
+    }
+    private void doAnimation(Animation anim){
+        linearContainer.startAnimation(anim);
+        drawerButton.startAnimation(anim);
+        sijaintiButton.startAnimation(anim);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng suomi = new LatLng(65.55, 25.55);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(suomi));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(suomi, 5));
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
