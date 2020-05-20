2ef87839d20c9f815a5fddabe740c841359e29b4
Saku Jamsa <t8jasa01@students.oamk.fi>
2020-05-20 11:11:40 +0300

    reitin haku lisätty, vaatii vielä reittivaihtoehdot

---
+++ app/src/main/java/com/example/carpool_app/SetRideConstant.java
@@ -0,0 +1,28 @@
+package com.example.carpool_app;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class SetRideConstant {
+
+    public static int pendingAlarmsAmount = 0;
+    public static long MinuteInMillis = 60000;
+    public static long HourInMillis = 3600000;
+    public static long DayInMillis = 86400000;
+
+    public static String DISTANCE = "";
+    public static String DURATION = "";
+    public static List<HashMap<String, String>> pointsList;
+    public static List<String> waypointAddressesList = new ArrayList<String>() {
+        {
+            add("");
+            add("");
+        }
+    };
+
+    public static void emptyPointsList() {
+        pointsList.clear();
+    }
+
+}
---
+++ app/src/main/java/com/example/carpool_app/SetRideTaskLoadedCallback.java
@@ -0,0 +1,5 @@
+package com.example.carpool_app;
+
+public interface SetRideTaskLoadedCallback {
+    void onTaskDone(Object... values);
+}
