4b9888f943c99ed81f363a6325120e5c48386e99
Saku Jamsa <t8jasa01@students.oamk.fi>
2020-06-22 12:46:27 +0300

    kyydin lisätiedot xml mupokkausta

--- app/src/main/java/com/example/carpool_app/SetRideDetailsActivity.java
+++ app/src/main/java/com/example/carpool_app/SetRideDetailsActivity.java
@@ -3,37 +3,46 @@ package com.example.carpool_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class SetRideDetailsActivity extends AppCompatActivity implements View.OnClickListener{
 
 
-    private CheckBox checkBox;
-    EditText lahtoaikaSanallinen;
+    private CheckBox checkBox_aika, checkBox_matkatavara;
+    EditText lahtoaikaSanallinen, matkatavaraSanallinen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_ride_details);
 
-        checkBox = (CheckBox) findViewById(R.id.setRideDetails_checkBox_aika);
-        checkBox.setOnClickListener(this);
+        checkBox_aika = (CheckBox) findViewById(R.id.setRideDetails_checkBox_aika);
+        checkBox_matkatavara = (CheckBox) findViewById(R.id.setRideDetails_checkBox_matkatavarat);
+        checkBox_aika.setOnClickListener(this);
+        checkBox_matkatavara.setOnClickListener(this);
         lahtoaikaSanallinen = (EditText)findViewById(R.id.setRideDetails_editText_sanallinenAika);
+        matkatavaraSanallinen = (EditText)findViewById(R.id.setRideDetails_editText_sanallinenTavaratila);
     }
 
     @Override
     public void onClick(View v) {
-        if(checkBox.isChecked())
+        if(checkBox_aika.isChecked())
         {
             lahtoaikaSanallinen.setVisibility(View.VISIBLE);
         }
         else
         {
             lahtoaikaSanallinen.setVisibility(View.GONE);
+        }
+        if(checkBox_matkatavara.isChecked())
+        {
+            matkatavaraSanallinen.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            matkatavaraSanallinen.setVisibility(View.GONE);
         }
     }
 }
